e04f104c2375e5449c5c57d9af550c86ff8124f7
nak02ee <cdp90@naver.com>
2019-10-04 12:36:30 +0900

    1004

--- app/src/main/java/com/leenak0/swurent/reservation_history.java
+++ app/src/main/java/com/leenak0/swurent/reservation_history.java
@@ -1,19 +1,29 @@
 package com.leenak0.swurent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.Toast;
-import androidx.appcompat.app.AlertDialog;
+import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
 
 public class reservation_history extends AppCompatActivity {
 
-    private TableLayout tableLayout;
+    TextView sampletxt, sampletxt2;
     Button rh_prev;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +31,50 @@ public class reservation_history extends AppCompatActivity {
         setContentView(R.layout.activity_reservation_history);
 
         rh_prev=(Button)findViewById(R.id.rh_prev);
-        tableLayout = (TableLayout) findViewById(R.id.table_layout);
-        tableLayout.setShrinkAllColumns(true);
+        sampletxt = (TextView)findViewById(R.id.sampletxt);
+        sampletxt2 = (TextView)findViewById(R.id.sampletxt2);
+
+        //파이어베이스에서부터 값을 받아오기
+
+
+
+        db.collection("회원예약내역")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                message = document.getId()+document.getData().toString();
+                                sampletxt.setText(message);
+                            }
+                        }
+                    }
+                });
+
+
+        DocumentReference docRef = db.collection("회원예약내역").document("2017111320");
+        Source source = Source.CACHE;
+
+        docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    String i = document.toString();
+                    //sampletxt2.setText(i);
+                }
+            }
+        });
+
+        //쿼리문 예시
+        //컬렉션 레퍼런스 생성
+        CollectionReference memberRef = db.collection("회원예약내역");
+        //컬렉션 쿼리
+        Query query = memberRef.whereArrayContains("2019년9월29일_11","50주년 기념관");
+        String result = query.toString();
+        sampletxt2.setText(result);
+        //com.google.firebase.firestore.Query@45658ceb
 
         rh_prev.setOnClickListener(new View.OnClickListener(){ //뒤로
 
@@ -32,39 +84,5 @@ public class reservation_history extends AppCompatActivity {
                 finish();
             }
         });
-    }
-
-    public void onBackPressed(View v) {
-        Toast.makeText(this, "뒤로가기 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
-        super.onBackPressed();
-    }
-
-    public void cancelButton(View v) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("예약 취소").setMessage("예약을 취소하시겠습니까?.");
-
-        builder.setPositiveButton("아니요", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "예약이 유지됩니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        builder.setNegativeButton("예", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Toast.makeText(getApplicationContext(), "예약이 취소되었습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    public void reservedButton(View v) {
-        Toast.makeText(getApplicationContext(), "예약 중입니다.", Toast.LENGTH_SHORT).show();
-    }
-
-    public void representButton(View v) {
-        Toast.makeText(getApplicationContext(), "대표자 성명 : 전세희", Toast.LENGTH_SHORT).show();
     }
 }
